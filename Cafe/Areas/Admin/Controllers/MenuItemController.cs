3390b06f4861dfd39eac70c9a9f6cf018cee2c57
khanishvili <khnshvl@gmail.com>
2019-08-27 01:29:16 -0400

    Menu Item Update post

--- Cafe/Areas/Admin/Controllers/MenuItemController.cs
+++ Cafe/Areas/Admin/Controllers/MenuItemController.cs
@@ -142,10 +142,10 @@ namespace Cafe.Areas.Admin.Controllers
             MenuViewModel.MenuItem.SubCategoryID = Convert.ToInt32(Request.Form["SubCategoryID"].ToString());
             if (!ModelState.IsValid)
             {
+                MenuViewModel.SubCategories = await dbContext.SubCategories.Where(s=>s.CategoryID ==MenuViewModel.MenuItem.CategoryID).ToListAsync();
                 return View(MenuViewModel);
             }
-            dbContext.MenuItems.Add(MenuViewModel.MenuItem);
-            await dbContext.SaveChangesAsync();
+ 
             //Saving Image Section
 
             string WebRootPath = hostingEnvironment.WebRootPath;
@@ -155,24 +155,32 @@ namespace Cafe.Areas.Admin.Controllers
             if (files.Count > 0)
 
             {
-                // files has been uploaded
+                // New Image will  upload
                 string uploads = Path.Combine(WebRootPath, "images");
-                string extension = Path.GetExtension(files[0].FileName);
-                using (FileStream fileStream = new FileStream(Path.Combine(uploads, MenuViewModel.MenuItem.ID + extension), FileMode.Create))
+                string extension_new = Path.GetExtension(files[0].FileName);
+
+                var imagepath = Path.Combine(WebRootPath, menuItemFromDb.Image.TrimStart('\\'));
+                if (System.IO.File.Exists(imagepath))
+                {
+                    System.IO.File.Delete(imagepath);
+                }
+                //we will upload new file
+                using (FileStream fileStream = new FileStream(Path.Combine(uploads, MenuViewModel.MenuItem.ID + extension_new), FileMode.Create))
                 {
                     files[0].CopyTo(fileStream);
                 }
 
-                menuItemFromDb.Image = @"\images\" + MenuViewModel.MenuItem.ID + extension;
+                menuItemFromDb.Image = @"\images\" + MenuViewModel.MenuItem.ID + extension_new;
             }
-            else
-            {
-                //no files was uploaded
 
-                var uploads = Path.Combine(WebRootPath, @"\images\" + SD.DefaultFoodImage);
-                System.IO.File.Copy(uploads, WebRootPath + @"\images\" + MenuViewModel.MenuItem.ID + ".jpg");
-                menuItemFromDb.Image = @"\images\" + MenuViewModel.MenuItem.ID + ".jpg";
-            }
+            menuItemFromDb.Name = MenuViewModel.MenuItem.Name;
+            menuItemFromDb.Description = MenuViewModel.MenuItem.Description;
+            menuItemFromDb.Price = MenuViewModel.MenuItem.Price;
+            menuItemFromDb.Taste = MenuViewModel.MenuItem.Taste;
+            menuItemFromDb.CategoryID = MenuViewModel.MenuItem.CategoryID;
+            menuItemFromDb.SubCategoryID = MenuViewModel.MenuItem.SubCategoryID;
+
+
             await dbContext.SaveChangesAsync();
 
             return RedirectToAction(nameof(Index));
